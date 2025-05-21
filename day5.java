7a4af09b65afa1e5b388c3c9e3ee67ce1215b126
faizzshaykh <faizzshaikh287>
2025-05-21 09:49:59 +0530

    thirdcommit

--- day5.java
+++ day5.java
@@ -1,3 +1,18 @@
-public class day5 {
-    
+class Solution {
+    public void reverseArray(int arr[]) {
+        // code here
+        int i=0;
+        int j=arr.length-1;
+        
+        while(i<j){
+            int temp=arr[i];
+            arr[i]=arr[j];
+            arr[j]=temp;
+             
+            i++;
+            j--;
+            
+        
+        }
+    }
 }
